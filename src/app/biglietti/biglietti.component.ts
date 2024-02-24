10a4ec72310b8157878c70053bfe23f65eb50e5c
tiberiu05 <tiberiu.popa97@gmail.com>
2024-02-24 18:46:08 +0100

    Agenzia del Lavoro

---
+++ src/app/biglietti/biglietti.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biglietti',
+  templateUrl: './biglietti.component.html',
+  styleUrls: ['./biglietti.component.css']
+})
+export class BigliettiComponent {
+
+}
