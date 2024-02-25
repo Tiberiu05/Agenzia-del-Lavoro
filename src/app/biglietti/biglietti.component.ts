b81d5580e383369fe62dd6193f3af55854d9c636
Mihaela Petruta Cipriano <mihaela.cipriano@gmail.com>
2024-02-25 09:36:26 +0100

    ts conpilato

--- src/app/biglietti/biglietti.component.ts
+++ src/app/biglietti/biglietti.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-biglietti',
   templateUrl: './biglietti.component.html',
   styleUrls: ['./biglietti.component.css']
 })
+
 export class BigliettiComponent {
+/* Definisco l'array di tipo String*/
+tabellone:String[]=[];
+coda:String[]=[];
+
+ultimoTicket=0;
+ultimoTicketFormattato='';
+elementiMaxTabellone=10;
+
+staccaTicket(){
+  this.ultimoTicket++;
+this.generaTiketFormattato();
+this.aggiungiTicketCoda();
+console.log(this.coda);
+}
+
+private generaTiketFormattato(){
+  let res= this.ultimoTicket.toString();
+  if(this.ultimoTicket<10){
+    res='00'+res
+  }else if(this.ultimoTicket<100){
+    res='0'+res;
+  }
+  this.ultimoTicketFormattato=res;
+}
+
+private aggiungiTicketCoda(){
+ this.coda.push(this.ultimoTicketFormattato);
+    
+  }
+
+gestisciCoda(){
+  const primoElementoCoda=this.coda.shift();
+  if(primoElementoCoda!==undefined){
+    this.tabellone.unshift(primoElementoCoda);
+  
+  }
+  if(this.tabellone.length<= this.elementiMaxTabellone){
+    this.tabellone.splice(this.elementiMaxTabellone)
+  }
+}  
+
 
 }
